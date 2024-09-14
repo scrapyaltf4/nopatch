8266734aed02959efae2e8055fe264dd0885b8d0
scrapyaltf4 <avansavakin@gmail.com>
2024-09-14 20:28:03 +0200

    install.bat

---
+++ install.bat
@@ -0,0 +1,14 @@
+@echo off
+pip install requests
+pip install pycryptodome
+pip install pystyle
+pip install colorama
+pip install auto_py_to_exe
+pip install pyinstaller
+pip install wmi
+pip install customtkinter
+pip install cryptography
+title Creal Builder
+cls
+echo Starting the builder now
+python builder.pyw
